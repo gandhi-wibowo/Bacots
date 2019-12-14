11b22405bb2ed1ba201bea0ff125c9805a5d1356
Gandhi Wibowo <gandhi@dartmedia.biz>
2019-12-15 04:01:33 +0700

    fixing lost .gradle

--- app/src/main/java/com/dizcoding/bacots/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/dizcoding/bacots/adapter/ViewPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.dizcoding.bacots.model.Fragments
 
-class ViewPagerAdapter (fm: FragmentManager,val list: List<Fragments>) : FragmentPagerAdapter(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+class ViewPagerAdapter (fm: FragmentManager, private val list: List<Fragments>) : FragmentPagerAdapter(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
     override fun getItem(position: Int): Fragment = list[position].fragment
     override fun getCount(): Int = list.size
     override fun getPageTitle(position: Int): CharSequence? = list[position].title
